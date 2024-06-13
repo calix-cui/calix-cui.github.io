e39a7818127f7bc0a5e26f9da5fcaec1862a7e17
CuiXiang <xiang.cui@modeling-tech.com>
2024-06-13 15:31:09 +0800

    try basePath = ""

--- blog/next.config.mjs
+++ blog/next.config.mjs
@@ -2,14 +2,14 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = "";
 let basePath = "";
 
-if (isGithubActions) {
-  // 去掉 `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+// if (isGithubActions) {
+//   // 去掉 `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
 
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-  console.log(basePath)
-}
+//   assetPrefix = `/${repo}/`;
+//   basePath = `/${repo}`;
+//   console.log(basePath)
+// }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
